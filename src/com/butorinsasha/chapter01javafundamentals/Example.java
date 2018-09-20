cf423a9267a60da4eecabf06b35e29e1d11c8c02
Aleksandr Butorin <butorinsasha@gmail.com>
2018-09-20 03:34:42 +0300

    chapter01javafundamentals.Example is done, git ititialized, .gitignored

---
+++ src/com/butorinsasha/chapter01javafundamentals/Example.java
@@ -0,0 +1,8 @@
+package com.butorinsasha.chapter01javafundamentals;
+
+public class Example {
+
+    public static void main(String[] args) {
+        System.out.println("Java drives the Web");
+    }
+}
