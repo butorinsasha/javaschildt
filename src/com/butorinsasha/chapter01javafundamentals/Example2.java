93a4f096d9b82491b9d9d7ee8330bb4b86ea1fc2
Aleksandr Butorin <butorinsasha@gmail.com>
2018-09-20 03:48:03 +0300

    second commit just for setting bare repository at butorinsasha gooledrive

---
+++ src/com/butorinsasha/chapter01javafundamentals/Example2.java
@@ -0,0 +1,7 @@
+package com.butorinsasha.chapter01javafundamentals;
+
+public class Example2 {
+    public static void main(String[] args) {
+        System.out.println("Java is super portable and secure!");
+    }
+}
