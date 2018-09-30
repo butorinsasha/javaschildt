4be85248347b0383434d43b3765a949f1dfa9350
Aleksandr Butorin <butorinsasha@gmail.com>
2018-09-30 19:30:13 +0300

    Example2.java var1 and var2

--- src/com/butorinsasha/chapter01javafundamentals/Example.java
+++ src/com/butorinsasha/chapter01javafundamentals/Example.java
@@ -1,7 +1,6 @@
 package com.butorinsasha.chapter01javafundamentals;
 
 public class Example {
-
     public static void main(String[] args) {
         System.out.println("Java drives the Web");
     }
--- src/com/butorinsasha/chapter01javafundamentals/Example2.java
+++ src/com/butorinsasha/chapter01javafundamentals/Example2.java
@@ -2,6 +2,16 @@ package com.butorinsasha.chapter01javafundamentals;
 
 public class Example2 {
     public static void main(String[] args) {
-        System.out.println("Java is super portable and secure!");
+
+        int var1;
+        int var2;
+
+        var1 = 1024;
+
+        System.out.println("var1 contains " + var1);
+
+        var2 = var1 / 2;
+        System.out.print("var2 contains var1 / 2: ");
+        System.out.println(var2);
     }
 }
