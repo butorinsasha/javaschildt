8e2a716c2431c8a2d286e53184fde0f941012b61
Aleksandr Butorin <butorinsasha@gmail.com>
2018-10-02 01:25:33 +0300

    chapter02introducingdatatypesandoperators is in progres

---
+++ src/chapter02introducingdatatypesandoperators/WallEcho.java
@@ -0,0 +1,10 @@
+package chapter02introducingdatatypesandoperators;
+
+public class WallEcho {
+    public static void main(String[] args) {
+        double t = 2.0;
+        double soundSpeed = 343; // in meters
+        double dist = t / 2 * soundSpeed;
+        System.out.println("Distance to the wall is " + dist + " meters");
+    }
+}
