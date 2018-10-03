a57b81e1ef5854999d9a1d61e4bcb1e685dce27d
butorin <butorin@spb.rusoft-company.ru>
2018-10-03 11:11:53 +0300

    modified PrimeNums inner loop condition from 'i < n' to 'i < n/2' but it is still not that efficient as it could be. searching why it should be 'i < n/i'

--- src/chapter02introducingdatatypesandoperators/PrimeNums.java
+++ src/chapter02introducingdatatypesandoperators/PrimeNums.java
@@ -9,7 +9,7 @@ public class PrimeNums {
 //        List<Integer> primes = new ArrayList<Integer>();
         for (int n = 2; n <= 100; n++) {
             boolean isPrime = true;
-            for (int i = 2; i < n; i++) {
+            for (int i = 2; i < n/2; i++) {
                 if (n % i == 0) {
                     isPrime = false;
                 }
