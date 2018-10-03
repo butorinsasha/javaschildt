acd5d422974b7f6466ac6cc1632333b857d86486
Aleksandr Butorin <butorinsasha@gmail.com>
2018-10-03 03:33:27 +0300

    chapter02 is done

--- src/chapter02introducingdatatypesandoperators/PrimeNums.java
+++ src/chapter02introducingdatatypesandoperators/PrimeNums.java
@@ -7,14 +7,16 @@ public class PrimeNums {
     public static void main(String[] args) {
 
 //        List<Integer> primes = new ArrayList<Integer>();
-
         for (int n = 2; n <= 100; n++) {
+            boolean isPrime = true;
             for (int i = 2; i < n; i++) {
                 if (n % i == 0) {
-                    break;
+                    isPrime = false;
                 }
             }
-            System.out.print(n + ", ");
+            if (isPrime) {
+                System.out.print(n + ", ");
+            }
         }
     }
 }
