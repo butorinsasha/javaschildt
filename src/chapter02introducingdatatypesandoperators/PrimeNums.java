88fa8ac7345fb3eca24d1a3dfdbb2fd7407b08af
butorin <butorin@spb.rusoft-company.ru>
2018-10-03 11:28:05 +0300

    PrimeNums is comprehend completely

--- src/chapter02introducingdatatypesandoperators/PrimeNums.java
+++ src/chapter02introducingdatatypesandoperators/PrimeNums.java
@@ -9,9 +9,13 @@ public class PrimeNums {
 //        List<Integer> primes = new ArrayList<Integer>();
         for (int n = 2; n <= 100; n++) {
             boolean isPrime = true;
-            for (int i = 2; i < n/2; i++) {
-                if (n % i == 0) {
+//            for (int i = 2; i <= n; i++) {
+//            for (int i = 2; i <= n/2; i++) {
+//            for (int i = 2; i <= n/i; i++) {
+            for (int i = 2; i <= Math.sqrt(n); i++) {
+                if ((n % i) == 0) {
                     isPrime = false;
+                    break;
                 }
             }
             if (isPrime) {
