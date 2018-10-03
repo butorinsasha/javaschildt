96cdc083a448cd2d2f8f6dc5bbf7e003ab427cd7
butorin <butorin@spb.rusoft-company.ru>
2018-10-03 13:11:18 +0300

    super effective algorithm in terms of time to find prime numbers

--- src/chapter02introducingdatatypesandoperators/LogicalOpTable.java
+++
@@ -1,7 +0,0 @@
-package chapter02introducingdatatypesandoperators;
-
-public class LogicalOpTable {
-    public static void main(String[] args) {
-        System.out.println("jajajaja");
-    }
-}
--- src/chapter02introducingdatatypesandoperators/PrimeNums.java
+++ src/chapter02introducingdatatypesandoperators/PrimeNums.java
@@ -4,23 +4,42 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PrimeNums {
-    public static void main(String[] args) {
 
-//        List<Integer> primes = new ArrayList<Integer>();
+    static int steps;
+
+    static boolean boolCheckStep(boolean b) {
+        steps++;
+        if (b) {
+            return true;
+        } else {
+            return false;
+        }
+//        System.out.println(steps);
+    }
+
+    public static void main(String[] args) {
+        steps = 0;
+        List<Integer> primes = new ArrayList<>();
         for (int n = 2; n <= 100; n++) {
             boolean isPrime = true;
 //            for (int i = 2; i <= n; i++) {
 //            for (int i = 2; i <= n/2; i++) {
+//            for (int i = 2; i <= Math.sqrt(n); i++) {
 //            for (int i = 2; i <= n/i; i++) {
-            for (int i = 2; i <= Math.sqrt(n); i++) {
-                if ((n % i) == 0) {
+            for (Integer i : primes) {
+                if ( (i <= n/i) && boolCheckStep((n % i) == 0) ) {
+//                if ( boolCheckStep((n % i) == 0) ) {
                     isPrime = false;
                     break;
                 }
             }
             if (isPrime) {
-                System.out.print(n + ", ");
+                primes.add(n);
+//                System.out.print(n + ", ");
             }
         }
+        System.out.println();
+        System.out.println("calculated for " + steps + " steps");
+//        System.out.println(primes);
     }
 }
