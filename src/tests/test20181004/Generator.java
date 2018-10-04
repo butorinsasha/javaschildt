47300d66c5a0cc187044fcde401e3b4097a5f8f0
Aleksandr Butorin <butorinsasha@gmail.com>
2018-10-05 00:27:01 +0300

    test20181004 Generator

---
+++ src/tests/test20181004/Generator.java
@@ -0,0 +1,35 @@
+package tests.test20181004;
+
+public class Generator {
+    int n;
+    int m;
+
+    public Generator(int m) {
+        this.m = m;
+    }
+
+    public static int countBinOnes(int n, int firstLowBits) {
+        int count = 0;
+        String binStr = Integer.toBinaryString(n);
+        if (binStr.length() >= 4) {
+
+            /* BUG FIX IN NEXT LINE */
+            for (int i = binStr.length() - 1; i >= (binStr.length() - firstLowBits); i--) {
+                if (binStr.charAt(i) == '1' )
+                    count++;
+            }
+        } else {
+            for (int i = 0; i < binStr.length(); i++) {
+                if (binStr.charAt(i) == '1' )
+                    count++;
+            }
+        }
+        return count;
+    }
+
+    public int getNext() {
+        this.m = (m * m * m) / countBinOnes(m, 4);
+        return this.m;
+    }
+
+}
---
+++ src/tests/test20181004/MyProgram.java
@@ -0,0 +1,16 @@
+package tests.test20181004;
+
+public class MyProgram {
+    public static void main(String[] args) {
+//        System.out.println("version 3.0");
+//        System.out.println(Integer.toBinaryString(Integer.parseInt(args[2])));
+//        System.out.println(Generator.countBinOnes(Integer.parseInt(args[2]),Integer.parseInt(args[3])));
+
+        Generator generate = new Generator(Integer.parseInt(args[1]));
+        System.out.print(args[1] + " ");
+        for (int i = 1; i <= ( Integer.parseInt(args[0]) - 1 ); i++) {
+            System.out.print(generate.getNext() + " ");
+        }
+        System.out.println();
+    }
+}
