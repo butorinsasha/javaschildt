a79bb56dc0e26a1888eaf5bd72485294849405a0
butorin <butorin@spb.rusoft-company.ru>
2018-10-17 17:19:30 +0300

    generics basics Pair class modified a bit

--- src/lects/lect20181004/Main.java
+++ src/lects/lect20181004/Main.java
@@ -3,6 +3,6 @@ package lects.lect20181004;
 public class Main {
     public static void main(String[] args) {
         Pair<Integer, String> p1 = new Pair<>(7, "hello");
-        Pair<Double, Pair<Integer,String>> p2 = new Pair<>(9.2, new Pair<Integer, String>(7, "hello"));
+        Pair<Double, Pair<Integer,String>> p2 = new Pair<>(9.2, new Pair<>(7, "hello"));
     }
 }
