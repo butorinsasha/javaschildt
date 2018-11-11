a94a991ae379d829087572067d0a716d4446bec3
Aleksandr Butorin <butorinsasha@gmail.com>
2018-11-12 02:43:05 +0300

    EuclideanDivision added quotion and remainder alternative

--- src/book/chapter04introducingclassesobjectsandmethods/EuclideanDivision.java
+++ src/book/chapter04introducingclassesobjectsandmethods/EuclideanDivision.java
@@ -2,13 +2,29 @@ package book.chapter04introducingclassesobjectsandmethods;
 
 public class EuclideanDivision {
 
+//    public static int quotion(int a, int b) {
+//      int q = (int) Math.signum(b) * Math.floorDiv(a, Math.abs(b));
+//      return q;
+//    }
+//
+//    public static int remainder(int a, int b) {
+//        int r = a - Math.abs(b) * Math.floorDiv(a, Math.abs(b));
+//        return r;
+//    }
+
     public static int quotion(int a, int b) {
-      int q = (int) Math.signum(b) * Math.floorDiv(a, Math.abs(b));
-      return q;
+        int q;
+        if (b > 0) q = (int) Math.floor( (int) (a / b) );
+        else if (b < 0) q = (int) Math.ceil( (int) (a / b) );
+        else throw new ArithmeticException("Division by zero");
+        return q;
     }
 
     public static int remainder(int a, int b) {
-        int r = a - Math.abs(b) * Math.floorDiv(a, Math.abs(b));
+        int r;
+
+        r = a - b * quotion(a,b);
+
         return r;
     }
 }
